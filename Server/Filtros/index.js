6f221d903f6b4b376d3555babbdf229889a8f8fd
Matti <mattibylin@Mattis-MacBook-Air.local>
2020-04-10 21:17:15 -0500

    Se crea función de filto ciudades en filtros/index.js

---
+++ Server/Filtros/index.js
@@ -0,0 +1,27 @@
+//Filtros
+//Import
+var bodyParser = require('body-parser')
+    http       = require('http')
+    express    = require('express')
+    Router     = express.Router()
+    jQuery     = require('jquery')
+    io         = require('socket.io')
+    Almacenamiento = require('../Almacenamiento')
+//Filtrar las ciudades de get info de almacenamiento que a su vez traen la info de data.json
+Router.get("/ciudades", function(req, res){
+  Almacenamiento.getInfo('ciudades')
+                .then(function(ciudades){
+                  var ciudadesUnique = [];
+                  ciudades.forEach(function(o){
+                    if(ciudadesUnique.indexOf(o.Ciudad)<0){
+                      ciudadesUnique.push(o.Ciudad);
+                    }
+                  })
+                  res.json(ciudadesUnique)
+                })
+                .catch(function(error){
+                  res.sendStatus(500).json(error);
+                  console.log('error filto ciudades')
+                })
+
+})
